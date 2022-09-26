68719ab6f533a2309e8dc1a3b0a73be2a2567bac
Dalton Daly <dalton.daly@yahoo.com>
2022-09-26 17:05:38 -0500

    Updates

---
+++ precache-manifest.79f5d8d401b4a2b2b93cc7986c50be21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c5f73742d2f1795b84",
+    "url": "/Penny-Pincher/static/css/main.9de919b2.chunk.css"
+  },
+  {
+    "revision": "05c5f73742d2f1795b84",
+    "url": "/Penny-Pincher/static/js/main.05c5f737.chunk.js"
+  },
+  {
+    "revision": "f0986bf8acc2a621da15",
+    "url": "/Penny-Pincher/static/js/1.f0986bf8.chunk.js"
+  },
+  {
+    "revision": "12a775f783fde991927b",
+    "url": "/Penny-Pincher/static/js/runtime~main.12a775f7.js"
+  },
+  {
+    "revision": "9dc49c5ea0014d2f3191f6a67467a615",
+    "url": "/Penny-Pincher/index.html"
+  }
+];
