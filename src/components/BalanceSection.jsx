444d4649969e0f8e20baf3ed58bbc356835a1389
Dalton Daly <104313049+ddaly2@users.noreply.github.com>
2022-10-08 12:13:42 -0500

    Added savings rate calculators

--- src/components/BalanceSection.jsx
+++ src/components/BalanceSection.jsx
@@ -8,6 +8,9 @@ function BalanceSection(props) {
       props.onCalculate(e);
      }}>Do Math</button>
      <p className="listItem">Your net income is: {props.value}</p>
+     <h3 className="balance-savings-item">Your suggested savings per month is: $ {(props.value) * .25}</h3>
+     <h3 className="balance-savings-item">Aggressive savings per month is: $ {(props.value) * .6}</h3>
+     <h3 className="balance-savings-item">Passive savings per month is: $ {(props.value) * .1}</h3>
     </div>
   )
 }
