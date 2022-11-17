9961f3e5b608bf04b71973fe9fa22459be5c7466
Dalton Daly <104313049+ddaly2@users.noreply.github.com>
2022-11-17 10:04:51 -0600

    Update BalanceSection.jsx

--- src/components/BalanceSection.jsx
+++ src/components/BalanceSection.jsx
@@ -3,14 +3,18 @@ import React from "react";
 function BalanceSection(props) {
   return (
     <div className="balanceSection">
-     <h2>Balance Section</h2>
-     <button type="submit" id="totalButton" onClick={(e) => {
-      props.onCalculate(e);
-     }}>Do Math</button>
-     <p className="listItem">Your net income is: {props.value}</p>
-     <h3 className="balance-savings-item">Your suggested savings per month is: $ {(props.value) * .25}</h3>
-     <h3 className="balance-savings-item">Aggressive savings per month is: $ {(props.value) * .6}</h3>
-     <h3 className="balance-savings-item">Passive savings per month is: $ {(props.value) * .1}</h3>
+     <div className="balanceSectionTop">
+        <h3>Balance Section</h3>
+        <button type="submit" id="balance-button" onClick={(e) => {
+        props.onCalculate(e);
+        }}>Find your net income</button>
+     </div>
+     <div className="balanceSectionBottom">
+        <h2 className="net-income">Your net income is: <span>$ {(props.value) * 1}</span></h2>
+        <h3 className="balance-savings-item">Suggested savings per month: <span>$ {Math.floor((props.value) * .25)}</span></h3>
+        <h3 className="balance-savings-item">Aggressive savings per month: <span>$ {Math.floor((props.value) * .6)}</span></h3>
+        <h3 className="balance-savings-item">Passive savings per month: <span>$ {Math.floor((props.value) * .1)}</span></h3>
+     </div>
     </div>
   )
 }
